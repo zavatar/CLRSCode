ff12756fd0d4317c7463cf439d262dd5a65b9f36
zhumeng1989 <zhumeng1989@gmail.com>
2012-10-19 22:25:57 +0800

    Add ModernC++.h

---
+++ CLRSCode/ModernC++.h
@@ -0,0 +1,101 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//	ModernC++.h
+//
+//	Written by Meng Zhu ,Zhejiang University (zhumeng1989@gmail.com)
+//	Creation date: Oct.19 2012, 16:46
+//
+//	All Rights Reserved.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef ModernCpp_H
+#define ModernCpp_H
+
+#include <cassert>
+#include <boost/static_assert.hpp>
+
+namespace mc {
+
+// Chapter 1. Policy-Based Class Design
+
+// Chapter 2. Techniques
+
+	// 2.1 Compile-Time Assertions
+	BOOST_STATIC_ASSERT(true);
+	// C++11
+	static_assert(true, "Msg");
+	
+	// 2.3 Local Classes
+	class Interface 
+	{ 
+	public: 
+		virtual void Fun() = 0; 
+	};
+	// Adapter
+	template <class T, class P> 
+	Interface* MakeAdapter(const T& obj, const P& arg) 
+	{ 
+		class Local : public Interface 
+		{ 
+		public: 
+			Local(const T& obj, const P& arg) 
+				: obj_(obj), arg_(arg) {} 
+			virtual void Fun() 
+			{ 
+				obj_.Call(arg_); 
+			} 
+		private: 
+			T obj_; 
+			P arg_; 
+		}; 
+		return new Local(obj, arg); 
+	}
+
+	// 2.6 Type Selection
+	template <bool flag, typename T, typename U> 
+	struct Select 
+	{
+		typedef T Result; 
+	}; 
+	template <typename T, typename U> 
+	struct Select<false, T, U> 
+	{ 
+		typedef U Result; 
+	};
+	template <typename T, bool isPolymorphic> 
+	class NiftyContainer 
+	{ 
+		typedef typename Select<isPolymorphic, T*, T>::Result 
+			ValueType; 
+	}; 
+
+	// 2.7 Detecting Convertibility and Inheritance at Compile Time
+	template <class T, class U> 
+	class Conversion 
+	{ 
+		typedef char Small; 
+		class Big { char dummy[2]; }; 
+		static Small Test(U);
+		static Big Test(...); 
+		static T MakeT(); 
+	public: 
+		enum { exists = 
+			sizeof(Test(MakeT())) == sizeof(Small) }; 
+		enum { sameType = false };
+	};
+	template <class T> 
+	class Conversion<T, T> 
+	{ 
+	public: 
+		enum { exists = 1, sameType = 1 }; 
+	}; 
+#define SUPERSUBCLASS(T, U) \
+	(::mc::Conversion<const U*, const T*>::exists && \
+		!::mc::Conversion<const T*, const void*>::sameType)
+#define SUPERSUBCLASS_STRICT(T, U) \
+		(SUPERSUBCLASS(T, U) && \
+		!::mc::Conversion<const T, const U>::sameType)
+}
+
+#endif
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ http://blog.csdn.net/zhumeng1989/article/category/1232333
 1. 项目中的函数实现都尽可能同原文一致，方便对照。包括名字大写这点。但由于原
 文数组索引从1开始，这点注意区别。
 2. 加入 Programming Pearls 编程珠玑 部分内容。
+3. 加入 Modern C++ Design C++设计新思维 部分内容。
